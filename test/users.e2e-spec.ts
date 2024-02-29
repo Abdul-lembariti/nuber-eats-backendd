430e078eba229993b875d5d0a82c28b081cba102
Abdul Lembariti <abdullembariti2005@gmail.com>
2024-02-29 08:28:19 +0300

    #10.3 Testing creating Account ||

---
+++ test/users.e2e-spec.ts
@@ -0,0 +1,96 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+import { Connection } from 'typeorm';
+import { getConnectionToken } from '@nestjs/typeorm';
+
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
+const GRAPHQL_ENDPOINT = '/graphql';
+
+describe('UserModule (e2e)', () => {
+  let app: INestApplication;
+  let connection: Connection;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+    app = module.createNestApplication();
+    await app.init();
+    connection = module.get<Connection>(getConnectionToken());
+  });
+
+  afterAll(async () => {
+    if (connection && connection.isConnected) {
+      await connection.dropDatabase();
+      await connection.close();
+    }
+    await app.close();
+  });
+
+  describe('createAccount', () => {
+    const EMAIL = 'nico@las.com';
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(input: {
+              email:"${EMAIL}",
+              password:"12345",
+              role:Owner
+            }) {
+              ok
+              error
+            }
+          }
+          `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    it('should fail if account already exists', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(input: {
+              email:"${EMAIL}",
+              password:"12345",
+              role:Owner
+            }) {
+              ok
+              error
+            }
+          }
+          `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toEqual(expect.any(String));
+        });
+    });
+  });
+
+  describe('userProfile', () => {
+    
+  });
+
+  it.todo('login');
+  it.todo('me');
+  it.todo('verifyEmail');
+  it.todo('editProfile');
+});
